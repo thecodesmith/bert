98045d25b7da916b78f595c2c113b9b99d01defb
Brian Stewart <brian@thecodesmith.com>
2015-11-05 16:13:33 -0600

    Removed sandbox testing files

--- src/main/groovy/com/thecodesmith/bert/sandbox/RunServer.groovy
+++
@@ -1,4 +0,0 @@
-package com.thecodesmith.bert
-
-server = new Server(4444)
-server.start()
--- src/main/groovy/com/thecodesmith/bert/sandbox/TestClient.groovy
+++
@@ -1,9 +0,0 @@
-s = new Socket('localhost', 4444)
-
-s.withStreams { inStream, outStream ->
-    outStream << "MAX_SPEED\n"
-    def buffer = inStream.newReader().readLine()
-    println "Received: ${buffer}"
-}
-
-s.close()
